36595d9a97d1ca587f840fe23c30d2d510921526
Andrew Locke <alocke12992@gmail.com>
2018-06-26 13:15:11 -0600

    Adds open-uri

--- app/controllers/parallel_controller.rb
+++ app/controllers/parallel_controller.rb
@@ -1,8 +1,7 @@
 class ParallelController < ApplicationController
   def parallelrate
-    require 'openssl'
-    doc = Nokogiri::HTML(open('https://www.abokifx.com/', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    binding.pry
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("https://www.abokifx.com/", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
     entries = doc.css('.lagos-market-rates-inner')
     rate = entries.css('table')[0].css('tr')[1].css('td')[1].text
     @formattedrate = rate[6..8]
