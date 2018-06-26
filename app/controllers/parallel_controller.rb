b3cf5efa9863962abeef54c8862d5c307581133b
Andrew Locke <alocke12992@gmail.com>
2018-06-26 12:32:58 -0600

    Builds parallel controller

--- app/controllers/parallel_controller.rb
+++ app/controllers/parallel_controller.rb
@@ -2,7 +2,7 @@ class ParallelController < ApplicationController
   def parallelrate
     require 'openssl'
     doc = Nokogiri::HTML(open('https://www.abokifx.com/', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-
+    binding.pry
     entries = doc.css('.lagos-market-rates-inner')
     rate = entries.css('table')[0].css('tr')[1].css('td')[1].text
     @formattedrate = rate[6..8]
